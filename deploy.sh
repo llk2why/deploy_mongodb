5b063846203a6b9c2845a8cf58d8e1bd9cc53245
lincoln <lincolnllklee@163.com>
2020-03-12 16:18:26 +0800

    quick-commit

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y python3-pip
-sudo apt install -y expect
-sudo apt install -y zip
-sudo apt install -y unzip
-sudo apt install -y python3-opencv
-sudo apt install -y sysstat 
-pip3 install pymongo
 
-curl -O https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.2.3.tgz
-tar -zxvf mongodb-linux-x86_64-ubuntu1804-4.2.3.tgz
-sudo mv mongodb-linux-x86_64-ubuntu1804-4.2.3/bin/* /usr/local/bin/
+# sudo apt update
+# sudo apt install -y python3-pip
+# sudo apt install -y expect
+# sudo apt install -y zip
+# sudo apt install -y unzip
+# sudo apt install -y python3-opencv
+# sudo apt install -y sysstat 
+# pip3 install pymongo
 
-cpu_num = $(grep 'core id' /proc/cpuinfo | sort -u | wc -l)
-repo_name = 'deploy_mongodb'
+# curl -O https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.2.3.tgz
+# tar -zxvf mongodb-linux-x86_64-ubuntu1804-4.2.3.tgz
+# sudo mv mongodb-linux-x86_64-ubuntu1804-4.2.3/bin/* /usr/local/bin/
+
+cpu_num=$(grep 'core id' /proc/cpuinfo | sort -u | wc -l)
+repo_name="deploy_mongodb"
 
 for((i=1;i<=3;i++))
 do
     sudo mkdir -p /home/mongod/config$i
 done
 
-for((i=1;i<=$($cpu_num);i++))
+for((i=1;i<=$cpu_num;i++))
 do
     sudo mkdir -p /home/mongod/shard$i
 done
@@ -48,14 +49,14 @@ do
 done
 
 python -m ~/$repo_name/shell_factory
-for((i=1;i<=$($cpu_num);i++))
+for((i=1;i<=$cpu_num;i++))
 do
     mongod -f ~/$repo_name/mongodbs_one_vm_config/shard$i.conf
 done
 
 mongos -f ~/$repo_name/mongodbs_one_vm_config/mongos.conf
 
-for((i=1;i<=$($cpu_num);i++))
+for((i=1;i<=$cpu_num;i++))
 do
     mongo --port 27017 --eval 'db.runCommand({\"addShard\":\"127.0.0.1:2702$i\" ,\"maxsize\":0,\"name\":\"shard$i\"}) admin'
 done
